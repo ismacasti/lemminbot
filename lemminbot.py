2b29ddee30fb8c486e20b4e9b320629dfb187dca
Ismael Castiñeira <ismael.castineira@udem.edu>
2016-12-03 05:38:02 +0200

    initial commit

---
+++ lemminbot.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+APIURL = "https://hyperion.nvf.io/latest-image/f437c149-5311-41f1-bddc-60369e69a000/"
+
+
+def main():
+    print("ok")
+    
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+    
